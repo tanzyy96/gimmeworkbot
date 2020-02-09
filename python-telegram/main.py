5c4bca100c62c4b4fdefabedc92a7b890acfd27b
tanzy <tan.zy096@gmail.com>
2020-02-09 13:15:30 +0800

    Created tele_env
    virtual environment for packages

--- python-telegram/main.py
+++ python-telegram/main.py
@@ -1,6 +1,6 @@
 from telegram.ext import Updater, InlineQueryHandler
 from telegram.ext import CommandHandler
-from telegram.ext import MessageHandler, Filters
+from telegram.ext import MessageHandler, Filters, CallbackContext
 from telegram import InlineQueryResultArticle, InputTextMessageContent
 import logging
 
@@ -10,32 +10,30 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 # TODO 01: hide the value of the token?
 updater = Updater(
     token="969883313:AAHS141QmPgF3B8OwYri-F7xY7FlGuH5TN0", use_context=True)
+j = updater.job_queue
 
 dispatcher = updater.dispatcher
-
-# basic command
 
 
 def start(update, context):
     context.bot.send_message(
         chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
+
 # using MessageHandler
-
-
 def echo(update, context):
     context.bot.send_message(
         chat_id=update.effective_chat.id, text=update.message.text)
 
+
 # using command arguments
-
-
 def caps(update, context):
     text_caps = ''.join(context.args).upper()
     context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
 
 
-def inline_caps(update, context):  # takes your inline input and return its capitilized form
+# takes your inline input and return its capitilized form
+def inline_caps(update, context):
     query = update.inline_query.query
     if not query:
         return
@@ -50,15 +48,30 @@ def inline_caps(update, context):  # takes your inline input and return its capi
     context.bot.answer_inline_query(update.inline_query.id, results)
 
 
+def unknown(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text="Sorry, I didn't understand that command.")
+
+
+def callback_minute(context: CallbackContext):
+    context.bot.send_message(chat_id='366058116',
+                             text='One message every minute')
+
+
 start_handler = CommandHandler('start', start)
 caps_handler = CommandHandler('caps', caps)
 echo_handler = MessageHandler(Filters.text, echo)
 inline_caps_handler = InlineQueryHandler(inline_caps)
+unknown_handler = MessageHandler(Filters.command, unknown)
 
 dispatcher.add_handler(start_handler)
 dispatcher.add_handler(caps_handler)
 dispatcher.add_handler(echo_handler)
 dispatcher.add_handler(inline_caps_handler)
+# must be added last, else all commands will trigger this handler
+dispatcher.add_handler(unknown_handler)
+
+job_minute = j.run_repeating(callback_minute, interval=60, first=0)
 
 updater.start_polling()
 print("Bot running...")
